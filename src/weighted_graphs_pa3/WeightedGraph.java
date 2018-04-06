5795832bb1477a2538aaecf3637b6293448f5a68
Timothy Small <timbo2016@hotmail.com>
2018-04-06 12:56:40 -0600

    First commit to get things started!

---
+++ src/weighted_graphs_pa3/WeightedGraph.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package weighted_graphs_pa3;
+
+/**
+ *
+ * @author timbo
+ */
+public class WeightedGraph {
+    
+    
+    public WeightedGraph(){
+        
+    }
+    
+    public MST Prim(){
+        
+        return null;
+    }
+    
+    public MST Kruskal(){
+        
+        return null;
+    }
+    
+    
+}

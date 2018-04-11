c67f10f72ccad1a3ce645fdf73ccf25b173e1b9b
James Jacobs <james.jacobs2@ecat.montana.edu>
2018-04-10 21:19:43 -0600

    slow progress on prim

--- src/weighted_graphs_pa3/MST.java
+++ src/weighted_graphs_pa3/MST.java
@@ -20,9 +20,33 @@ public class MST {
     
     
     
-    public MST Prim(){
+    public MST Prim(WeightedGraph graph, int s){
+        MST m = new MST(graph);
+        Heap q = graph.depthSearch();
         
-        return null;
+        Edge[] v = graph.getEdges();
+        Edge[] p;
+        
+        //for each member of q set the priority to "infinity"
+        for(Edge i : v){
+            //set the priority to "infinity"
+            v[i] = ; //?????
+        }
+        //set the starting vertex 's' priority to 0
+        //v[s] = null;
+        //parent of s should be null
+        
+        
+        while(!q.isEmpty()){
+            //get miniumum for q as u
+            //Vertex u = ;
+            //for each adjacent vertex v to u
+                //if v is in q and weight of (u,v) < priority of v then
+                //the parent of v is set to be u
+                //the priority of v is the weight of (u,v)
+        }
+        
+        return m;
     }
     
     public MST Kruskal(WeightedGraph graph){
@@ -39,58 +63,4 @@ public class MST {
         
         return copy;
     }
-    
-    public static final int NUMVERTS = 7;
-    
-    /*public int findMinimum(int vert[], Boolean mst[]){
-        int min = Integer.MAX_VALUE;
-        int minIndex = -1;
-        
-        for(int i = 0; i < NUMVERTS; i++){
-            if(mst[i] == false && vert[i] < min){
-                min = vert[i];
-                minIndex = i;
-            }
-        }
-        return minIndex;
-    }*/
-    // Translating the pseudocode into java. Everything here well be very temporary as things unravel
-    public MST Prim(int matrix[][]){
-        
-        /*int p[] = new int[NUMVERTS];  //probably needs to be vertex array but int for testing purposes
-        int vert[] = new int[NUMVERTS];
-        Boolean mst[] = new Boolean[NUMVERTS];
-        
-        for(int i = 0; i < NUMVERTS; i++){
-            vert[i] = Integer.MAX_VALUE;
-            mst[i] = false;
-        }
-        
-        vert[0] = 0;
-        p[0] = -1;
-        
-        for(int i = 0; i < NUMVERTS-1; i++){
-            int u = findMinimum(vert, mst);
-            
-            mst[u] = true;
-            
-            for(int j = 0; j < NUMVERTS; j++){
-                if(matrix[u][j] != 0 && mst[j] == false && matrix[u][j] < vert[j]){
-                    p[j] = u;
-                    vert[j] = matrix[u][j];
-                }
-            }
-        }
-        printP(p, NUMVERTS, matrix);*/
-        
-        return null;
-    }
-    /*public void printP(int p[], int n, int matrix[][]){
-        for(int i = 0; i < NUMVERTS; i++){
-            for(int j = 0; j < NUMVERTS; j++){
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }*/
 }
